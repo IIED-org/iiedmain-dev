8a9c7dc25bd81e07b52db5535eae590171b63afc
Nick Turner <nick.turner@iied.org>
2024-03-25 15:08:46 +0000

    updates

---
+++ dist/js/copycite.js
@@ -0,0 +1,28 @@
+document.getElementById("copy-button").addEventListener("click", function() {
+    var copyText = document.getElementById("citation-text");
+    // Create a new text area
+    var textArea = document.createElement("textarea");
+    textArea.value = copyText.textContent;
+    // Prevents scrolling to the bottom of the page in MS Edge.
+    textArea.style.all = 'unset';
+    textArea.style.position = 'fixed';
+    textArea.style.top = 0;
+    textArea.style.clip = 'rect(0, 0, 0, 0)';
+    textArea.style.whiteSpace = 'pre';
+    textArea.style.webkitUserSelect = 'text';
+    // Adds the text area and selects the text.
+    document.body.appendChild(textArea);
+    textArea.select(); 
+
+    try {
+        document.execCommand('copy');
+        // Alert statement removed from here
+    } catch (err) {
+        console.error('Failed to copy text to clipboard', err);
+    } finally {
+        document.body.removeChild(textArea);
+    }
+    // Change the icon after copying
+    document.getElementById("copy-button").style.display = "none";
+    document.getElementById("copied-icon").style.display = "block";
+});
